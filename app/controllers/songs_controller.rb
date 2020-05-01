7eb9d2bb72549284d27f10f9f7d1907f3946622a
Ariel <arielord@buffalo.edu>
2020-05-01 07:45:28 +0000

    Automatically backed up by Learn

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -26,7 +26,7 @@ class SongsController < ApplicationController
 
   def new
     @preference = Preference.first
-    if @Preference.allow_create_artists
+    if @Preference.allow_create_songs
       @song = Song.new
     else
       redirect_to songs_path
